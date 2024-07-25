a056dd6954d5847a84219b9e25baf9b692ded2f0
simone menichetti <simon3menichettui95@gmail.com>
2024-07-25 14:28:13 +0200

    build:(add path index php , start mamp and add container and form group with bootstrap)

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-password-generator</title>
+    <!-- bootstrap -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class='container'>
+        <div class='row justify-content-center'>
+            <div class='col-md-6'>
+                <div class='card mt-5'>
+                    <div class='card-body'>
+                        <h1 class='card-title text-center'>Generatore di Password casuali PHP</h1>
+                        <!-- creazione form  action  con processo in GET -->
+                        <form action="" method='GET'>
+                            <div class='form-group'>
+                                <label for="length">Lunghezza della password:</label>
+                                <!-- inserimento input -->
+                                <input type="submit" class='btn btn-primary' value='Genera la password'>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
