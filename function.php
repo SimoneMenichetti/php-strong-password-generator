db50565e3c4d509cdff00821d29faa08abf908dd
simone menichetti <simon3menichettui95@gmail.com>
2024-07-25 15:43:38 +0200

    feat:(add newpage function.php and transfer logic function , use  include  in index php for use logic)

---
+++ function.php
@@ -0,0 +1,33 @@
+<?php
+
+// inizializzo una funzione per generare automaticamente una password
+    function CreateRandomPassword($length){
+        // caratteri/simboli possibili ammessi nelle password
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
+        // password campo da popolare
+        $password= '';
+        // lunghezza della password 
+        $charactersLength = strlen($characters);
+
+        // inseriamo un controllo della lunghezza dei caratteri 
+        // var_dump($charactersLength);
+
+        // inizializiamo un ciclo for per generare casualmente la password
+
+        for($i = 0; $i < $length; $i++){
+            // utilizziamo rand per dare un valore minimo ed un massimo x i caratteri casuali da selezionare
+            $randomI=rand(0,$charactersLength-1);
+            // controllo del valore var indice random
+            // var_dump($randomI);
+
+            $password .= $characters[$randomI];
+
+            // controllo password
+            // var_dump($password);
+        }
+
+        // ritorniamo la password generata
+        return $password;
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,34 +1,38 @@
 <?php
 
-    // inizializzo una funzione per generare automaticamente una password
-    function CreateRandomPassword($length){
-        // caratteri/simboli possibili ammessi nelle password
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
-        // password campo da popolare
-        $password= '';
-        // lunghezza della password 
-        $charactersLength = strlen($characters);
+// includiamo la pagina function.php
 
-        // inseriamo un controllo della lunghezza dei caratteri 
-        // var_dump($charactersLength);
+include 'function.php';
 
-        // inizializiamo un ciclo for per generare casualmente la password
+    // // inizializzo una funzione per generare automaticamente una password
+    // function CreateRandomPassword($length){
+    //     // caratteri/simboli possibili ammessi nelle password
+    //     $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
+    //     // password campo da popolare
+    //     $password= '';
+    //     // lunghezza della password 
+    //     $charactersLength = strlen($characters);
 
-        for($i = 0; $i < $length; $i++){
-            // utilizziamo rand per dare un valore minimo ed un massimo x i caratteri casuali da selezionare
-            $randomI=rand(0,$charactersLength-1);
-            // controllo del valore var indice random
-            // var_dump($randomI);
+    //     // inseriamo un controllo della lunghezza dei caratteri 
+    //     // var_dump($charactersLength);
 
-            $password .= $characters[$randomI];
+    //     // inizializiamo un ciclo for per generare casualmente la password
 
-            // controllo password
-            // var_dump($password);
-        }
+    //     for($i = 0; $i < $length; $i++){
+    //         // utilizziamo rand per dare un valore minimo ed un massimo x i caratteri casuali da selezionare
+    //         $randomI=rand(0,$charactersLength-1);
+    //         // controllo del valore var indice random
+    //         // var_dump($randomI);
 
-        // ritorniamo la password generata
-        return $password;
-    }
+    //         $password .= $characters[$randomI];
+
+    //         // controllo password
+    //         // var_dump($password);
+    //     }
+
+    //     // ritorniamo la password generata
+    //     return $password;
+    // }
 
     // verifica della lunghezza della password settata
 
